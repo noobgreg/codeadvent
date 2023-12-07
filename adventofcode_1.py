e45bf969c50cf0867600993918c1674ef6c218c8
noobgreg <greks2404@gmail.com>
2023-12-07 22:07:08 +0100

    initial commit

--- adventofcode_1.py
+++ adventofcode_1.py
@@ -24,7 +24,10 @@ with open('input.txt', 'r') as file:
 
 # print(sum(all_numbers))
 
-pattern = r'\b(?:one|two|three|four|five|six|seven|eight|nine)\b'
+# pattern = r'\b(?:one|two|three|four|five|six|seven|eight|nine)\b'
+pattern = re.compile(r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))')
+digit_mapping = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+
 all_written_numbers = []
 
 with open('input.txt', 'r') as file:
@@ -33,16 +36,19 @@ with open('input.txt', 'r') as file:
         if not line:
             break
 
-        num = re.findall(pattern, line, flags=re.IGNORECASE)
+        num = pattern.findall(line)
+        converted_digits = [str(digit_mapping[string]) if string in digit_mapping else string for string in num]
         print(line)
         print(num)
+        print(converted_digits)
 
-        # if len(num) < 2:
-            # str = num[0] + num[0]
-        # else:
-            # str = num[0] + num[len(num)-1]
+        if len(converted_digits) < 2:
+            result = converted_digits[0] + converted_digits[0]
+        else:
+            result = converted_digits[0] + converted_digits[len(converted_digits)-1]
 
-        all_written_numbers.append(str)
+        all_written_numbers.append(result)
 
-
+#print(sum(all_written_numbers))
+print(sum(map(int, all_written_numbers)))
 print('end')
